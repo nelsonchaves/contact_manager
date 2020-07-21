0671e3deb009503ac9df4d919ca585a7fb3bd8b0
Nelson Chaves <nelsonchavespro@gmail.com>
2020-07-21 11:21:12 -0700

    Added update feature

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -26,6 +26,16 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
   end
 
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      flash[:sucess] = "Contact was successfully updated"
+      redirect_to contacts_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
   def contact_params
